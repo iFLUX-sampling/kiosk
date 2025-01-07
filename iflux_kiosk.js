1c5512b44bbf8a894db2346a926dfe8ac30d5054
nielsvanputte <46300683+nielsvanputte@users.noreply.github.com>
2025-01-07 10:51:09 +0100

    Create iflux_kiosk.js

---
+++ iflux_kiosk.js
@@ -0,0 +1,44 @@
+{
+  "settingsReloadIntervalMinutes": 1,
+  "fullscreen": true,
+  "autoStart": true,
+  "lazyLoadTabs": false,
+  "closeExistingTabs": false,
+  "websites": [
+ //   {
+ //     "url": "https://timeseer.fluxsense.app/tenant/~/",
+ //     "duration": 30,
+ //     "tabReloadIntervalSeconds": 600
+    },
+    {
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/12",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    },
+    {
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/13",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    },
+	    {
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/14",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    },
+{
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/16",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    },{
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/17",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    },{
+      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/19",
+      "duration": 10,
+      "tabReloadIntervalSeconds": 600
+    }
+  ]
+}
+        
+JSON: Valid
