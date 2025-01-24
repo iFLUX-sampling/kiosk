59135210acfc6c9975be8147c8f51b689ee96e8f
Bob Van Looveren <bobvanlooveren@protonmail.com>
2025-01-24 15:27:34 +0100

    Update iflux_kiosk.js
    Added Bankbusters, Luikbeek, Beverhoutsveld

--- iflux_kiosk.js
+++ iflux_kiosk.js
@@ -1,37 +1,55 @@
 {
-  "settingsReloadIntervalMinutes": 1,
-  "fullscreen": true,
-  "autoStart": true,
-  "lazyLoadTabs": false,
-  "closeExistingTabs": false,
-  "websites": [
-    {
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/12",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    },
-    {
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/13",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    },
-	    {
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/14",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    },
-{
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/16",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    },{
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/17",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    },{
-      "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/19",
-      "duration": 15,
-      "tabReloadIntervalSeconds": 600
-    }
-  ]
+    "settingsReloadIntervalMinutes": 1,
+    "fullscreen": true,
+    "autoStart": true,
+    "lazyLoadTabs": false,
+    "closeExistingTabs": false,
+    "websites":
+    [
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/12",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/13",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/14",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/16",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/17",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/19",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/31",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/33",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        },
+        {
+            "url": "https://timeseer.fluxsense.app/tenant/~/catalog/data-services/35",
+            "duration": 15,
+            "tabReloadIntervalSeconds": 600
+        }
+    ]
 }
